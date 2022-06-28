d0a855c6a77d43bafad35db4e5b20b48c889e32d
Sergio Sacchetti Junior <sacchettif13@gmail.com>
2022-06-28 19:08:47 -0400

    Arrumado acesso do index e carregamentos static

--- zSetup/urls.py
+++ zSetup/urls.py
@@ -1,8 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from geral.views import Index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('', Index.as_view(), name='index'),
 ]
