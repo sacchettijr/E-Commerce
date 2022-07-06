1b95e8da26b6a457ae11b16e5dde2199ffdad92f
Sergio Sacchetti Junior <sacchettif13@gmail.com>
2022-07-06 11:21:01 -0400

    Criado fixtures dos produtos

--- run_fixtures.bat
+++ run_fixtures.bat
@@ -2,3 +2,6 @@ python manage.py loaddata pais
 python manage.py loaddata estado
 python manage.py loaddata cidade
 python manage.py loaddata usuario
+python manage.py loaddata produto_categoria
+python manage.py loaddata produto
+python manage.py loaddata produto_imagem
