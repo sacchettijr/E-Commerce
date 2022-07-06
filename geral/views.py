66b2d945f8db3bddc49d1963881dbdaea28a9c36
Sergio Sacchetti Junior <sacchettif13@gmail.com>
2022-07-06 12:02:34 -0400

    Feito a página index

--- geral/views.py
+++ geral/views.py
@@ -1,10 +1,15 @@
 from django.views.generic import TemplateView
 
+from produto.models import ProdutoCategoria
 
+
+# VIEWS PUBLICAS
 class Index(TemplateView):
-	template_name = 'index.html'
+	template_name = 'publico/index.html'
 	
 	def get_context_data(self, context=None, **kwargs):
-		self.kwargs['categorias'] = Categoria.objects.filter(
+		self.kwargs['categorias'] = ProdutoCategoria.objects.filter(
 			ativo=True)  # Navbar
 		return self.kwargs
+
+# VIEWS DE GERAL
