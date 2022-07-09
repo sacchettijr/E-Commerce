4a00f9a32abea078cf31efd11765350e24d1bc91
Sergio Sacchetti Junior <sacchettif13@gmail.com>
2022-07-09 16:09:12 -0400

    Criado e direcionado listagem e detalhe dos produtos

--- geral/views.py
+++ geral/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 
-from produto.models import ProdutoCategoria
+from produto.models import ProdutoCategoria, Produto, ProdutoImagem
 
 
 # VIEWS PUBLICAS
@@ -8,8 +8,29 @@ class Index(TemplateView):
 	template_name = 'publico/index.html'
 	
 	def get_context_data(self, context=None, **kwargs):
-		self.kwargs['categorias'] = ProdutoCategoria.objects.filter(
-			ativo=True)  # Navbar
+		self.kwargs['categorias'] = ProdutoCategoria.objects.filter(ativo=True)  # Navbar
 		return self.kwargs
 
+
+class PublicoCategoriaView(TemplateView):
+	template_name = 'publico/publico_produto_listagem.html'
+	
+	def get_context_data(self, context=None, **kwargs):
+		self.kwargs['categorias'] = ProdutoCategoria.objects.filter(ativo=True)  # Navbar
+		categoria_id = ProdutoCategoria.objects.get(slug=kwargs['slug']).pk
+		self.kwargs['categoria_nome'] = ProdutoCategoria.objects.get(slug=kwargs['slug']).nome
+		self.kwargs['produtos'] = Produto.objects.filter(categoria=categoria_id, ativo=True)
+		self.kwargs['produtos_imagens'] = ProdutoImagem.objects.filter(padrao=True)
+		return self.kwargs
+
+
+class PublicoProdutoDetalheView(TemplateView):
+	template_name = 'publico/publico_produto_detalhe.html'
+	
+	def get_context_data(self, context=None, **kwargs):
+		self.kwargs['categorias'] = ProdutoCategoria.objects.filter(ativo=True)  # Navbar
+		produto = Produto.objects.get(slug=kwargs['slug'])
+		self.kwargs['produto'] = produto
+		self.kwargs['produtos_imagens'] = ProdutoImagem.objects.filter(produto=produto.pk, ativo=True)
+		return self.kwargs
 # VIEWS DE GERAL
--- kathrynestore/urls.py
+++ kathrynestore/urls.py
@@ -2,13 +2,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from geral.views import Index
+from geral.views import Index, PublicoCategoriaView, PublicoProdutoDetalheView
 from usuario.views import UsuarioCadastro
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
-	path('api-auth/', include('rest_framework.urls')),
+	# DJANGO
+	path('admin/', admin.site.urls), path('api-auth/', include('rest_framework.urls')),
+	
+	# USUÁRIO
 	path('accounts/', include('django.contrib.auth.urls')),
-	path('', Index.as_view(), name='index'),
 	path("accounts/cadastro/", UsuarioCadastro.as_view(), name="signup"),
+	
+	# PUBLICO
+	path('', Index.as_view(), name='index'),
+	path('categoria/<slug:slug>/', PublicoCategoriaView.as_view(), name='publico_categoria'),
+	path('produto/<slug:slug>/', PublicoProdutoDetalheView.as_view(), name='publico_produto'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
